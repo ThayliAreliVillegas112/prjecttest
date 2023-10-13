b613425cc70f7e2d45ddc0fb26fa8bff866dae33
ThayliAreliVillegas112 <83984948+ThayliAreliVillegas112@users.noreply.github.com>
2023-10-12 21:42:30 -0600

    Update count.dart

--- lib/modules/count/adapter/screens/count.dart
+++ lib/modules/count/adapter/screens/count.dart
@@ -12,31 +12,33 @@ class Count extends StatefulWidget{ //genera un estado a _CountState
 class _CountState extends State<Count> {
   int number =1;
   final random = Random();
-  int indice =0;
+  Color actualColor = Colors.blue;
+  int randomColor =0;
   List<Color> colors = [Colors.blue, Colors.yellow, Colors.pink, Colors.purple, Colors.black];
   void cambiarColor(){
-    var nuevocolor = random.nextInt(colors.length);
+    //actualColor = colors[random.nextInt(colors.length)];
+    randomColor = (currentNumberColorIndex + 1) % colors.length;
   }
   @override
   Widget build(BuildContext context) {
-    TextStyle fontTouch = const TextStyle(fontSize: 32.0, color: Color(0xFF7C42CA));
+    TextStyle fontTouch = const TextStyle(fontSize: 32.0, color: colors[randomColor]);
     
     return Scaffold(
-      appBar: AppBar(title: const Text("Contador Thayli"),),
+      appBar: AppBar(title: const Text("Contador Thayli"), backgroundColor: colors[randomColor],),
       body:  Center( //centra bien
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, //centra en vertical
           crossAxisAlignment: CrossAxisAlignment.center, // centra en horizontal
           children: [
-            Text('Número de touch', style: fontTouch),
-            Text('$number', style: fontTouch)
+            Text('Número de touch', style: color: colors[randomColor]),
+            Text('$number', style: color: colors[randomColor])
           ]
         ),
       ),
       //floatingActionButton: const Text("Plus"),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(onPressed:(){
-        
+        cambiarColor();
         number++;
         print('Hola, $number'); 
         setState((){});}, 
